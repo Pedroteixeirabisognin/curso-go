9cc4a79911a3e838ee86c6940884684650d498da
pedroteixeirabisognin <pedroteixeirabisognin@gmail.com>
2021-07-20 16:16:07 -0300

    tipos em go

--- tipos/metodos/metodos.go
+++ tipos/metodos/metodos.go
@@ -14,6 +14,7 @@ func (p pessoa) getNomeCompleto() string {
 	return p.nome + " " + p.sobrenome
 }
 
+//Necessário usar ponteiro para poder alterar o struct
 func (p *pessoa) setNomeCompleto(nomeCompleto string) {
 	partes := strings.Split(nomeCompleto, " ")
 	p.nome = partes[0]
@@ -24,6 +25,7 @@ func main() {
 	p1 := pessoa{"Pedro", "Silva"}
 	fmt.Println(p1.getNomeCompleto())
 
+	//Não precisa mudar a chamada ele já entende que é para pegar o ponteiro
 	p1.setNomeCompleto("Maria Pereira")
 	fmt.Println(p1.getNomeCompleto())
 }
---
+++ tipos/pseudoheranca/pseudoheranca.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+type carro struct {
+	nome            string
+	velocidadeAtual int
+}
+
+type ferrari struct {
+	carro       //campos anonimos
+	turboLigado bool
+}
+
+func (c carro) getNome() string {
+	return c.nome
+}
+
+func main() {
+	f := ferrari{}
+	f.nome = "F40"
+	f.velocidadeAtual = 0
+	f.turboLigado = true
+
+	fmt.Printf("A ferrari %s está com turbo ligado? %v\n", f.nome, f.turboLigado)
+	fmt.Printf("\nO nome do carro é: %s", f.getNome())
+
+}
